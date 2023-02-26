c05095b9f3a462e62bb8dc87d899f75b1e675007
HariPrasath12051997 <122340623+HariPrasath12051997@users.noreply.github.com>
2023-02-26 12:28:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let resume={
+    name : "Hari Prasath",
+    Age : 25,
+    Place : "Karur",
+    Qualification:"MBA",
+    college : "VSB ENGINEERING COLLEGE",
+    Experience : "Analyst",
+    ExperiencePeriod : 1,
+    // Passed out : 2018,
+};
+console.log(resume);
